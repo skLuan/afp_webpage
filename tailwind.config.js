9866d7aba1506c5c04d4393f58850c3712dd7b7f
Luan E <luan.erazo@skyrocketlab.space>
2022-12-22 13:56:11 -0500

    fonts instaled

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                black: {
+                    DEFAULT: "#1a1a1a",
+                    true: "#000000",
+                },
+                grey: {
+                    DEFAULT: "#4D4D4D",
+                    light: "#898989",
+                },
+                yellow: {
+                    DEFAULT: "#f5a832",
+                },
+                white: {
+                    DEFAULT: "#f5f3f0",
+                    bg: "#edebe8",
+                    true: "#ffffff",
+                },
+            },
+            fontFamily: {
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+                lemon: ["LemonMilk"],
+                jost: ["Jost"],
+            },
+            height: {
+                11: "42px",
+            },
+            textShadow: {
+                afp: "-2px 2px 3px rgba(0, 0, 0, 0.25)",
+            },
+        },
+    },
+
+    plugins: [require("@tailwindcss/forms"), require("tailwindcss-textshadow")],
+};
