89b906e7987a4b449325738e000992815abc2013
Luan E <luan.erazo@skyrocketlab.space>
2023-06-23 13:43:04 -0500

    habilitando el correo a todos

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -56,8 +56,8 @@ class Controller extends BaseController
         $msn           = $request->input('notes');
 
         if ($this->reCapcha($request)) {
-            // Mail::to('grover.vargas@americanflooringproducts.com')->cc($this->users)->bcc('erazo.luan@gmail.com')->send(new ForAdmin($projectName, $ubication, $projecDimentions, $customerName, $email, $phone, $msn));
-            Mail::to('erazo.luan@gmail.com')->send(new ForAdmin($projectName, $ubication, $projecDimentions, $customerName, $email, $phone, $msn));
+            Mail::to('grover.vargas@americanflooringproducts.com')->cc($this->users)->bcc('erazo.luan@gmail.com')->send(new ForAdmin($projectName, $ubication, $projecDimentions, $customerName, $email, $phone, $msn));
+            // Mail::to('erazo.luan@gmail.com')->send(new ForAdmin($projectName, $ubication, $projecDimentions, $customerName, $email, $phone, $msn));
             return redirect()->back()->with('success', '#contact');
         } else {
             return redirect()->back()->with('error', '#contact');
